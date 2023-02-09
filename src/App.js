610c8838bc6608613040a52b2af399e62a64e6b7
Pavel Shuvalov <shuvalov.pavel@gmail.com>
2023-02-09 10:13:12 +0300

    Add top jettons dynamic page

--- src/App.js
+++ src/App.js
@@ -1,14 +1,40 @@
 import logo from './logo.png';
 import './App.css';
+import TopJettons from "./TopJettons";
+import {Box, Container, Grid, Link, Typography} from "@mui/material";
+import GitHubIcon from '@mui/icons-material/GitHub';
+import TelegramIcon from '@mui/icons-material/Telegram';
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        <img src={logo} className="App-logo" alt="logo" style={{maxHeight:200}}/>
+        <Typography variant="h4">
           Real-time security & operational monitoring
-        </p>
+        </Typography>
+        <TopJettons/>
+        <Typography variant="h3">
+          More TON Jetton insights coming soon...
+        </Typography>
+        <Container>
+          <Grid container spacing={2}>
+            <Grid item xs={4}>
+              <Box sx={{m: 7}}>
+                <GitHubIcon className="service_icon" color="primary"/>
+              </Box>
+              <Link href="https://github.com/re-doubt" variant="h4">@re-doubt</Link>
+            </Grid>
+            <Grid item xs={4}/>
+            <Grid item xs={4}>
+              <Box sx={{m: 7}}>
+                <TelegramIcon className="service_icon" color="primary"/>
+              </Box>
+              <Link href="https://t.me/re_doubt" variant="h4">@re_doubt</Link>
+            </Grid>
+          </Grid>
+        </Container>
       </header>
     </div>
   );
---
+++ src/TopJettons.js
@@ -0,0 +1,105 @@
+import {
+  Box, CircularProgress,
+  Container, Grid,
+  Paper,
+  Table, TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Typography
+} from "@mui/material";
+import {useEffect, useState} from "react";
+import NewIcon from '@mui/icons-material/FiberNew';
+
+const API_ROOT = process.env.REACT_APP_API_ROOT ?? "https://api.redoubt.online/";
+
+function ValueWithTrend(props) {
+  const {value, percent} = props.data;
+  return (
+    <Grid container wrap="nowrap">
+      <Grid xs="auto">
+        <Typography variant="h6">{props.formatter ? props.formatter(value) : value}</Typography>
+      </Grid>
+      <Grid xs={1}/>
+      <Grid xs="auto">
+        {percent === null ? (
+          <NewIcon color="primary" className="value_new"/>
+        ) : (<Typography variant="h6" className={`value_${percent > 0 ? 'growth' : 'decrease'}`}>
+          {percent} %
+        </Typography>)}
+      </Grid>
+    </Grid>
+  )
+}
+
+function Jetton(props) {
+  const { jetton } = props;
+  return (
+    <TableRow>
+      <TableCell className="jettons_cell">
+        <Grid container wrap="nowrap">
+          <Grid xs="auto">
+            <img src={`${process.env.REACT_APP_API_ROOT}v1/jettons/image/${jetton.address}`}
+                 className="jetton_icon" alt={jetton.name} id={`image_${jetton.address}`}/>
+          </Grid>
+          <Grid xs={8}><Typography variant="h6">{jetton.name}</Typography></Grid>
+          <Grid xs={2}></Grid>
+        </Grid>
+
+      </TableCell>
+      <TableCell className="jettons_cell"><ValueWithTrend formatter={d => d.toPrecision(4)} data={jetton.price}/></TableCell>
+      <TableCell className="jettons_cell"><ValueWithTrend data={jetton.marketVolume}/></TableCell>
+      <TableCell className="jettons_cell"><ValueWithTrend data={jetton.activeOwners24h}/></TableCell>
+      <TableCell className="jettons_cell"><ValueWithTrend data={jetton.totalHolders}/></TableCell>
+      <TableCell className="jettons_cell">
+        {jetton.sinceCreationSeconds > 86400 * 30 ?
+        <Typography variant="h6">1+&nbsp;month&nbsp;ago</Typography> :
+        <Typography variant="h6">{(jetton.sinceCreationSeconds / 86400).toFixed(0)}&nbsp;days&nbsp;ago</Typography>
+        }
+      </TableCell>
+    </TableRow>
+  )
+}
+
+function TopJettons(props) {
+  const [jettons, setJettons] = useState([]);
+
+  useEffect(() => {
+    fetch(API_ROOT + 'v1/jettons/top', { method: 'GET' })
+      .then(response => response.json())
+      .then(res => {
+        setJettons(res['jettons']);
+      })
+      .catch(error => console.warn(error));
+  }, [])
+
+  return (
+    <Container sx={{pt: 5, pb:6}}>
+      <TableContainer>
+        <Table sx={{ minWidth: 650, color: 'text.primary', borderCollapse: 'separate', borderSpacing: '0px 10px' }} size="small" aria-label="a dense table" className="jettons_table">
+          <TableHead>
+            <TableRow>
+              <TableCell className="jettons_cell">Jetton</TableCell>
+              <TableCell className="jettons_cell">Price (TON / jetton)</TableCell>
+              <TableCell className="jettons_cell">Volume 24h (TON)</TableCell>
+              <TableCell className="jettons_cell">Active owners 24h</TableCell>
+              <TableCell className="jettons_cell">Total holders</TableCell>
+              <TableCell className="jettons_cell">Created</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {
+             jettons !== undefined ? jettons.map((jetton, idx) => (<Jetton
+               key={idx}
+               jetton={jetton}
+             />)) : <CircularProgress/>
+            }
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </Container>
+  )
+}
+
+export default TopJettons;
